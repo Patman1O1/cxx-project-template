8d3e0b28f8151c940e2a258b97b864307aa9ea70
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-17 16:03:51 -0600

    Edit executable_definition.cmake

--- .github/cmake/executable_definition.cmake
+++ .github/cmake/executable_definition.cmake
@@ -7,11 +7,11 @@ set_target_properties(${PROJECT_PRIMARY_TARGET} PROPERTIES
 )
 
 # Create objects
-add_library(PROJECT_OBJECTS OBJECT "${CMAKE_SOURCE_DIR}/src/${PROJECT_PRIMARY_TARGET}/${PROJECT_PRIMARY_TARGET}.cpp")
-add_library(${PROJECT_NAMESPACE}::PROJECT_OBJECTS ALIAS PROJECT_OBJECTS)
+add_library(${PROJECT_PRIMARY_TARGET}_objects OBJECT "${CMAKE_SOURCE_DIR}/src/${PROJECT_PRIMARY_TARGET}/${PROJECT_PRIMARY_TARGET}.cpp")
+add_library(${PROJECT_NAMESPACE}::${PROJECT_PRIMARY_TARGET}_objects ALIAS ${PROJECT_PRIMARY_TARGET}_objects)
 
 # Include directories
-target_include_directories(PROJECT_OBJECTS
+target_include_directories(${PROJECT_PRIMARY_TARGET}_objects
         PRIVATE
             "${CMAKE_SOURCE_DIR}/include/${PROJECT_PRIMARY_TARGET}"
         PUBLIC
@@ -22,18 +22,18 @@ target_include_directories(PROJECT_OBJECTS
 # Set sources
 target_sources(${PROJECT_PRIMARY_TARGET}
         PRIVATE
-        "${CMAKE_SOURCE_DIR}/src/${PROJECT_PRIMARY_TARGET}/main.cpp"
-        $<TARGET_OBJECTS:PROJECT_OBJECTS>
+            "${CMAKE_SOURCE_DIR}/src/${PROJECT_PRIMARY_TARGET}/main.cpp"
+            $<TARGET_OBJECTS:${PROJECT_PRIMARY_TARGET}_objects>
 )
 
 # Link objects with the primary target
-target_link_libraries(${PROJECT_PRIMARY_TARGET} PRIVATE PROJECT_OBJECTS)
+target_link_libraries(${PROJECT_PRIMARY_TARGET} PRIVATE ${PROJECT_PRIMARY_TARGET}_objects)
 
 # Define installation rules
 if(NOT CMAKE_SKIP_INSTALL_RULES)
     include(GNUInstallDirs)
 
-    install(TARGETS ${PROJECT_PRIMARY_TARGET}
+    install(TARGETS ${PROJECT_PRIMARY_TARGET} ${PROJECT_PRIMARY_TARGET}_objects
             RUNTIME COMPONENT ${PROJECT_PRIMARY_TARGET}
             DESTINATION "${CMAKE_INSTALL_BINDIR}"
     )
